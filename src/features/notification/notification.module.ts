beeefeaa92b9ea5957ab4a426e4258e18c660dde
Kacper Geisheimer <devkarer@gmail.com>
2019-06-10 14:18:52 +0200

    Verify and decode notification response

--- src/features/notification/notification.module.ts
+++ src/features/notification/notification.module.ts
@@ -1,16 +1,34 @@
+import crypto from 'crypto'
+
 import { LibModule } from '../../utils'
+import { WirecardError } from '../../error'
 
 import { EncryptedNotificationRequest, NotificationRequest, Notification } from '.'
 
 export class NotificationModule extends LibModule {
   public consume(encryptedNotification: EncryptedNotificationRequest): Notification {
-    const request: NotificationRequest = this.decrypt(encryptedNotification)
+    const request: NotificationRequest = this.verify(encryptedNotification)
 
     return this.mapRequestToNotification(request)
   }
 
-  protected decrypt(notification: EncryptedNotificationRequest): NotificationRequest {
-    return {} as NotificationRequest
+  protected verify(notification: EncryptedNotificationRequest): NotificationRequest {
+    const config = this.getConfig()
+
+    const signature = crypto
+      .createHmac('sha256', config.secretKey)
+      .update(notification['response-base64'])
+      .digest('hex')
+
+    const expectedSignature = new Buffer(notification['response-signature-base64'], 'base64').toString('hex')
+
+    if (signature !== expectedSignature) {
+      throw new WirecardError(`Notification signature verification failed. Expected ${expectedSignature} and got ${signature}`)
+    }
+
+    const json = new Buffer(notification['response-base64'], 'base64').toString('utf8')
+
+    return JSON.parse(json) as NotificationRequest
   }
 
   protected mapRequestToNotification(request: NotificationRequest): Notification {
