fa41c139aea45c9062f59ee7d6682475acc98319
Kacper Geisheimer <devkarer@gmail.com>
2019-06-10 14:35:43 +0200

    Map notification request to notification

--- src/features/notification/notification.module.ts
+++ src/features/notification/notification.module.ts
@@ -32,6 +32,45 @@ export class NotificationModule extends LibModule {
   }
 
   protected mapRequestToNotification(request: NotificationRequest): Notification {
-    return {} as Notification
+    const { payment } = request
+
+    const status = request.statuses.status[0]
+
+    return {
+      payment: {
+        transactionType: payment['transaction-type'],
+        transactionId: payment['transaction-id'],
+        completionTimeStamp: payment['completion-time-stamp'],
+        cardToken: {
+          tokenId: payment['card-token']['token-id'],
+          maskedAccountNumber: payment['card-token']['masked-account-number']
+        },
+        merchantAccountId: payment['merchant-account-id'].value,
+        transactionState: payment['transaction-state'],
+        paymentMethods: {
+          paymentMethod: payment['payment-methods']['payment-method'].map((e): string => e.name)
+        },
+        cancelUrl: payment['cancel-redirect-url'],
+        successUrl: payment['success-redirect-url'],
+        errorUrl: payment['fail-redirect-url'],
+        apiId: payment['api-id']
+      },
+      requestId: request['request-id'],
+      requestedAmount: {
+        currency: request['requested-amount'].currency,
+        value: request['requested-amount'].value
+      },
+      status: {
+        description: status.description,
+        severity: status.severity,
+        code: status.code
+      },
+      authorizationCode: request['authorization-code'],
+      accountHolder: {
+        firstName: request['account-holder']['first-name'],
+        lastName: request['account-holder']['last-name']
+      },
+      descriptor: request.descriptor
+    }
   }
 }
--- src/features/notification/notification.ts
+++ src/features/notification/notification.ts
@@ -1,5 +1,36 @@
 export interface Notification {
-  placeholder: string // todo
+  payment: {
+    transactionType: string
+    transactionId: string
+    completionTimeStamp: string
+    cardToken: {
+      tokenId: string
+      maskedAccountNumber: string
+    }
+    merchantAccountId: string
+    transactionState: string
+    paymentMethods: {
+      paymentMethod: string[]
+    }
+    cancelUrl: string
+    successUrl: string
+    errorUrl: string
+    apiId: string
+  }
+  requestId: string
+  requestedAmount: {
+    currency: string
+    value: number
+  }
+  status: {
+    description: string
+    severity: string
+    code: string
+  }
+  authorizationCode: string
+  accountHolder: {
+    firstName: string
+    lastName: string
+  }
+  descriptor: string
 }
-
-export type NotificationPromise = Promise<Notification>
